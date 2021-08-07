4da3420656dcebdc3ec89ab60ce31bc9f1d21381
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-08-07 10:51:37 +0530

    initial

---
+++ dist/server.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+
+var ejs = require('ejs');
+
+var path = require('path');
+
+var expressLayout = require('express-ejs-layouts');
+
+var PORT = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+  res.render('home');
+}); //set template engine
+
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+app.listen(3000, function () {
+  console.log("Listening on port ".concat(PORT));
+});
+//# sourceMappingURL=server.dev.js.map
