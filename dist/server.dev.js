1075287e17bfa8de87a06c64e4124902ccf72059
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-08-08 13:20:20 +0530

    Added register and login

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -12,14 +12,23 @@ var expressLayout = require('express-ejs-layouts');
 
 var PORT = process.env.PORT || 3000; //Assets
 
-app.use(express["static"]('public'));
-app.get('/', function (req, res) {
-  res.render('home');
-}); //set template engine
+app.use(express["static"]('public')); //set template engine
 
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+app.get('/', function (req, res) {
+  res.render('home');
+});
+app.get('/cart', function (req, res) {
+  res.render('customers/cart');
+});
+app.get('/login', function (req, res) {
+  res.render('auth/login');
+});
+app.get('/register', function (req, res) {
+  res.render('auth/register');
+});
 app.listen(3000, function () {
   console.log("Listening on port ".concat(PORT));
 });
